5ee972e64f28efb9cc22f6b70aca048e36bb6321
Annirudh Prasad <annirudh@wandb.com>
2020-10-02 11:36:51 -0700

    more fix

--- wandb/errors/term.py
+++ wandb/errors/term.py
@@ -51,7 +51,6 @@ def termwarn(string, **kwargs):
 
 
 def termerror(string, **kwargs):
-    silent = env.get_log_level() == ""
     string = '\n'.join(['{} {}'.format(ERROR_STRING, s)
                         for s in string.split('\n')])
     termlog(string=string, newline=True, **kwargs)
